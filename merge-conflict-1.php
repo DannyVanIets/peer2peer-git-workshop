323b6bc0a1ce91d2d51d0bd1959c3aa4bcfbf13b
Danny van Bokhorst <amonkeyeatingicecream@hotmail.com>
2021-09-26 20:18:09 +0200

    Merge conflict veroorzaken
    Gezellig

--- merge-conflict-1.php
+++ merge-conflict-1.php
@@ -2,5 +2,6 @@
     echo "Today is " . date("Y/m/d") . "<br>";
     echo "Today is " . date("Y.m.d") . "<br>";
     echo "Today is " . date("Y-m-d") . "<br>";
-    echo "Today is " . date("l");
+    echo "Today is " . date("l") . "<br>";
+    &copy; 2010-<?php echo date("Y");?>
 ?>
